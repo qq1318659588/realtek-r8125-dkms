c20b0053a2442ca3179b244fff1c8f80188c0959
qq1318659588 <40486058+qq1318659588@users.noreply.github.com>
2023-05-20 14:00:20 +0800

    Create 0_init_pve_header.sh
    add init pve header

---
+++ 0_init_pve_header.sh
@@ -0,0 +1,7 @@
+
+apt install -y devscripts debmake debhelper build-essential dkms  pve-headers-$(uname -r)
+
+ln -sf  /usr/src/linux-headers-$(uname -r)/   /usr/lib/modules/$(uname -r)/build
+
+# cd dkms-driver-home ; auto build driver deb and install 
+# dpkg-buildpackage -b -rfakeroot -us -uc
